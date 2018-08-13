f7ef8ea1a782657db8eb2bb60ff651fba698cb89
Bahadır Zeren <36567337+bahadrzeren@users.noreply.github.com>
2018-08-13 17:58:02 +0100

    Director class is removed!

--- src/main/java/org/heuros/ssim2csv/Ssim2Legs.java
+++ src/main/java/org/heuros/ssim2csv/Ssim2Legs.java
@@ -1,12 +1,10 @@
 package org.heuros.ssim2csv;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.heuros.conf.HeurosConfFactory;
-import org.heuros.core.base.Loader;
-import org.heuros.core.base.Reporter;
 import org.heuros.data.model.LegImpl;
-import org.heuros.director.DataTransformDirector;
 import org.heuros.loader.ssim.SsimLoader;
 import org.heuros.reporter.legcsv.LegCsvReporter;
 
@@ -30,16 +28,19 @@ public class Ssim2Legs {
 								.createConfObject(confFileName, HeurosConf.class);
 
 		if (conf != null) {
+			/**
+			 * Load input data.
+			 */
+			List<LegImpl> legs = new SsimLoader().setSsimFileName(conf.getSsim())
+														.setAcRotationFileName(conf.getRotation())
+														.setCarryInFileName(conf.getCarryIn())
+														.extractData();
 
-			Loader<LegImpl> loader = new SsimLoader(conf.getSsim(),
-												conf.getRotation(),
-												conf.getCarryIn());
-
-			Reporter<LegImpl> reporter = new LegCsvReporter(conf.getOutput());
-
-			new DataTransformDirector<LegImpl>().registerLoader(loader)
-											.registerReporter(reporter)
-											.proceed();
+			/**
+			 * Convert input data into CSV format.
+			 */
+			new LegCsvReporter().setLegCsvReporter(conf.getOutput())
+								.reportData(legs);
 		}
     }
 }
